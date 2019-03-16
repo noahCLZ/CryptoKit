c7e88fe3d0df31f5ba4874e4b13f6687ddc220b9
noahCLZ <noah.cl.zhuang@gmail.com>
2019-03-16 16:51:15 +0800

    Initial Commit

---
+++ CryptoKit/CryptoKit.h
@@ -0,0 +1,19 @@
+//
+//  CryptoKit.h
+//  CryptoKit
+//
+//  Created by noah.cl.zhuang on 2019/3/16.
+//  Copyright © 2019 noah.cl.zhuang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CryptoKit.
+FOUNDATION_EXPORT double CryptoKitVersionNumber;
+
+//! Project version string for CryptoKit.
+FOUNDATION_EXPORT const unsigned char CryptoKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CryptoKit/PublicHeader.h>
+
+
