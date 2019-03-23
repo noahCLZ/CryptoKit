0572368bc3dcff234aa6518b389b39495a053a18
noahCLZ <noah.cl.zhuang@gmail.com>
2019-03-23 09:18:33 +0800

    Add cocoapods support

---
+++ Pods/Target Support Files/Pods-CryptoKit-tvOS/Pods-CryptoKit-tvOS-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_CryptoKit_tvOS : NSObject
+@end
+@implementation PodsDummy_Pods_CryptoKit_tvOS
+@end
---
+++ Pods/Target Support Files/Pods-CryptoKit-watchOS/Pods-CryptoKit-watchOS-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_CryptoKit_watchOS : NSObject
+@end
+@implementation PodsDummy_Pods_CryptoKit_watchOS
+@end
