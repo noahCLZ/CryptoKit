60b6e64f6a748b4a0f36dd21e9179b6121e11d98
noahCLZ <noah.cl.zhuang@gmail.com>
2019-03-23 22:53:19 +0800

    Remove pod integration

--- Pods/Target Support Files/Pods-CryptoKit-iOS/Pods-CryptoKit-iOS-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_CryptoKit_iOS : NSObject
-@end
-@implementation PodsDummy_Pods_CryptoKit_iOS
-@end
--- Pods/Target Support Files/Pods-CryptoKit-tvOS/Pods-CryptoKit-tvOS-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_CryptoKit_tvOS : NSObject
-@end
-@implementation PodsDummy_Pods_CryptoKit_tvOS
-@end
